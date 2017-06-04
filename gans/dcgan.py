ad62f358fcd18518e0147cb16b56f81e09485295
Yixing Lao <yixing.lao@gmail.com>
2017-06-04 16:36:46 -0700

    some annotation of sizes

--- gans/dcgan.py
+++ gans/dcgan.py
@@ -119,27 +119,30 @@ class _netG(nn.Module):
     def __init__(self, ngpu):
         super(_netG, self).__init__()
         self.ngpu = ngpu
+        # torch.nn.ConvTranspose2d(in_channels, out_channels, kernel_size,
+        #                          stride=1, padding=0, output_padding=0,
+        #                          groups=1, bias=True, dilation=1)
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
+            # input is Z, going into a convolution: [64, 100, 1, 1]
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*8) x 4 x 4: [64, 512, 4, 4]
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # state size. (ngf*4) x 8 x 8: [64, 256, 8, 8]
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf*2) x 16 x 16: [64, 128, 16, 16]
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf) x 32 x 32: [64, 64, 32, 32]
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # state size. (nc) x 64 x 64: [64, 3, 64, 64]
         )
 
     def forward(self, input):
@@ -163,7 +166,7 @@ class _netD(nn.Module):
         super(_netD, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
+            # input is (nc) x 64 x 64: [64, 3, 64, 64]
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
