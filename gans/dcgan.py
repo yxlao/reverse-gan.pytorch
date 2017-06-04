dbbd60b32622c148c836787461d64a6b64da2626
Yixing Lao <yixing.lao@gmail.com>
2017-06-04 16:43:53 -0700

    some annotation of sizes in D

--- gans/dcgan.py
+++ gans/dcgan.py
@@ -169,21 +169,22 @@ class _netD(nn.Module):
             # input is (nc) x 64 x 64: [64, 3, 64, 64]
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            # state size. (ndf) x 32 x 32: [64, 64, 32, 32]
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*2) x 16 x 16: [64, 128, 16, 16]
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+            # state size. (ndf*4) x 8 x 8: [64, 256, 8, 8]
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
+            # state size. (ndf*8) x 4 x 4: [64, 512, 4, 4]
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
+            # output size: [64, 1, 1, 1]
         )
 
     def forward(self, input):
