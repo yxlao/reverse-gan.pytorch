9f2ad4bd1f000092a232b3ee262f452a2f46ccac
Yixing Lao <yixing.lao@gmail.com>
2017-06-04 21:58:15 -0700

    default 2gpu 20 thread

--- dcgan.py
+++ dcgan.py
@@ -19,7 +19,7 @@ parser.add_argument('--dataset', required=True,
                     help='cifar10 | lsun | imagenet | folder | lfw')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--workers', type=int,
-                    help='number of data loading workers', default=5)
+                    help='number of data loading workers', default=20)
 parser.add_argument('--batchSize', type=int, default=64,
                     help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64,
@@ -35,7 +35,7 @@ parser.add_argument('--lr', type=float, default=0.0002,
 parser.add_argument('--beta1', type=float, default=0.5,
                     help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
-parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--ngpu', type=int, default=2, help='number of GPUs to use')
 parser.add_argument('--netG', default='',
                     help="path to netG (to continue training)")
 parser.add_argument('--netD', default='',
@@ -275,7 +275,7 @@ def main():
                 print('Time elapsed: %.2f' % (curr_time - start_time))
                 start_time = curr_time
 
-            if opt.profile and i == 200:
+            if opt.profile and i == 500:
                 exit(0)
 
         # do checkpointing
