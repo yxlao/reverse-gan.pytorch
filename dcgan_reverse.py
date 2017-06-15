99670e44145e44ed5fcceefceea043ccf26551ae
Yixing Lao <yixing.lao@gmail.com>
2017-06-15 02:08:39 -0700

    refactor reverse_z to a function

--- dcgan_reverse.py
+++ dcgan_reverse.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import argparse
-import os
 import random
 import torch
 import torch.nn as nn
@@ -9,87 +8,97 @@ import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
 import torchvision.utils as vutils
-import time
 from torch.autograd import Variable
-import numpy as np
-
-
 from dcgan import NetG
 
-def reverse_gan(opt):
-    # process arguments
-    if torch.cuda.is_available() and not opt.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run "
-              "with --cuda")
 
-    if opt.manualSeed is None:
-        opt.manualSeed = random.randint(1, 10000)
-    print("Random Seed: ", opt.manualSeed)
-    random.seed(opt.manualSeed)
-    torch.manual_seed(opt.manualSeed)
+def reverse_z(netG, g_z, z, opt):
+    """
+    Estimate z_approx given G and G(z).
+
+    Args:
+        netG: nn.Module, generator network.
+        g_z: Variable, G(z).
+        opt: argparse.Namespace, network and training options.
+        z: Variable, the ground truth z, ref only here, not used in recovery.
+
+    Returns:
+        Variable, z_approx, the estimated z value.
+    """
+    # loss metrics
+    mse_loss = nn.MSELoss()
+    mse_loss_ = nn.MSELoss()
+
+    # init tensor
+    if opt.z_distribution == 'uniform':
+        z_approx = torch.FloatTensor(1, opt.nz, 1, 1).uniform_(-1, 1)
+    elif opt.z_distribution == 'normal':
+        z_approx = torch.FloatTensor(1, opt.nz, 1, 1).normal_(0, 1)
+    else:
+        raise ValueError()
+
+    # transfer to gpu
     if opt.cuda:
-        torch.cuda.manual_seed_all(opt.manualSeed)
-        cudnn.benchmark = True  # turn on the cudnn autotuner
-        # torch.cuda.set_device(1)
+        mse_loss.cuda()
+        mse_loss_.cuda()
+        z_approx = z_approx.cuda()
 
+    # convert to variable
+    z_approx = Variable(z_approx)
+    z_approx.requires_grad = True
+
+    # optimizer
+    optimizer_approx = optim.Adam([z_approx], lr=opt.lr,
+                                  betas=(opt.beta1, 0.999))
+
+    # train
+    for i in range(opt.niter):
+        g_z_approx = netG(z_approx)
+        mse_g_z = mse_loss(g_z_approx, g_z)
+        mse_z = mse_loss_(z_approx, z)
+        if i % 100 == 0:
+            print("[Iter {}] mse_g_z: {}, MSE_z: {}"
+                  .format(i, mse_g_z.data[0], mse_z.data[0]))
+
+        # bprop
+        optimizer_approx.zero_grad()
+        mse_g_z.backward()
+        optimizer_approx.step()
+
+    # save g(z_approx) image
+    vutils.save_image(g_z_approx.data, 'g_z_approx.png', normalize=True)
+
+
+def reverse_gan(opt):
     # load netG and fix its weights
     netG = NetG(opt.ngpu, opt.nz, opt.ngf, opt.nc)
     netG.load_state_dict(torch.load(opt.netG))
     for param in netG.parameters():
         param.requires_grad = False
 
-    # init ground-truth z and z_approx
+    # init z
     if opt.z_distribution == 'uniform':
-        noise = torch.FloatTensor(opt.batch_size, opt.nz, 1, 1).uniform_(-1, 1)
-        noise_approx = \
-            torch.FloatTensor(opt.batch_size, opt.nz, 1, 1).uniform_(-1, 1)
+        z = torch.FloatTensor(1, opt.nz, 1, 1).uniform_(-1, 1)
     elif opt.z_distribution == 'normal':
-        noise = torch.FloatTensor(opt.batch_size, opt.nz, 1, 1).normal_(0, 1)
-        noise_approx = \
-            torch.FloatTensor(opt.batch_size, opt.nz, 1, 1).normal_(0, 1)
+        z = torch.FloatTensor(1, opt.nz, 1, 1).normal_(0, 1)
     else:
         raise ValueError()
-    noise = Variable(noise)
-    noise.data.resize_(opt.batch_size, opt.nz, 1, 1)
+    z = Variable(z)
+    z.data.resize_(1, opt.nz, 1, 1)
 
-    for param in netG.parameters():
-        param.requires_grad = False
-
-    # fix fake, and try to find noise_approx
-    mse_loss = nn.MSELoss()
-    mse_loss_ = nn.MSELoss()
-
+    # transfer to gpu
     if opt.cuda:
         netG.cuda()
-        mse_loss.cuda()
-        mse_loss_.cuda()
-        noise, noise_approx = noise.cuda(), noise_approx.cuda()
+        z = z.cuda()
 
-    noise_approx = Variable(noise_approx)
-    noise_approx.requires_grad = True
+    # generate g_z
+    g_z = netG(z)
+    vutils.save_image(g_z.data, 'g_z.png', normalize=True)
+    print(z.cpu().data.numpy().squeeze())
 
-    # generate ground-truth fake
-    fake = netG(noise)
-
-    optimizer_approx = optim.Adam([noise_approx], lr=0.01, betas=(opt.beta1, 0.999))
-
-    for i in range(opt.niter):
-        fake_approx = netG(noise_approx)
-        mse_g_z = mse_loss(fake_approx, fake)
-        mse_z = mse_loss_(noise_approx, noise)
-        if i % 100 == 0:
-            print("[Iter {}] MSE_FAKE: {}, MSE_Z: {}".format(i, mse_g_z.data[0],
-                                                             mse_z.data[0]))
-
-        optimizer_approx.zero_grad()
-        mse_g_z.backward()
-        optimizer_approx.step()
-
-    vutils.save_image(fake.data, 'fake.png', normalize=True)
-    vutils.save_image(fake_approx.data, 'fake_approx.png', normalize=True)
-
-    print(noise.cpu().data.numpy().squeeze())
-    print(noise_approx.cpu().data.numpy().squeeze())
+    # recover z_approx from g_z
+    z_approx = reverse_z(netG, g_z, z, opt)
+    print(z_approx.cpu().data.numpy().squeeze())
 
 
 if __name__ == '__main__':
@@ -100,11 +109,8 @@ if __name__ == '__main__':
                         help='size of the latent z vector')
     parser.add_argument('--nc', type=int, default=3,
                         help='number of channels in the generated image')
-    parser.add_argument('--batch_size', type=int, default=1,
-                        help='input batch size, should be 1 since we reverse '
-                             'single images')
     parser.add_argument('--ngf', type=int, default=64)
-    parser.add_argument('--niter', type=int, default=100000,
+    parser.add_argument('--niter', type=int, default=10000,
                         help='number of epochs to train for')
     parser.add_argument('--lr', type=float, default=0.01,
                         help='learning rate, default=0.0002')
@@ -123,4 +129,20 @@ if __name__ == '__main__':
 
     opt = parser.parse_args()
     print(opt)
+
+    # process arguments
+    if torch.cuda.is_available() and not opt.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run "
+              "with --cuda")
+
+    if opt.manualSeed is None:
+        opt.manualSeed = random.randint(1, 10000)
+    print("Random Seed: ", opt.manualSeed)
+    random.seed(opt.manualSeed)
+    torch.manual_seed(opt.manualSeed)
+    if opt.cuda:
+        torch.cuda.manual_seed_all(opt.manualSeed)
+        cudnn.benchmark = True  # turn on the cudnn autotuner
+        # torch.cuda.set_device(1)
+
     reverse_gan(opt)
